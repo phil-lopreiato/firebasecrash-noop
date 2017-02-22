5fec652e6166bdd3e2a7238ad2b0902bffdca2ba
Phil Lopreiato <plnyyanks@gmail.com>
2017-02-22 12:03:57 -0500

    Basic FirebaseCrash file that does nothing

---
+++ lib/src/main/java/com/google/firebase/crash/FirebaseCrash.java
@@ -0,0 +1,16 @@
+package com.google.firebase.crash;
+
+public class FirebaseCrash {
+
+    public static void log(String message) {
+        // noop
+    }
+
+    public static void logcat(int leve, String tag, String message) {
+        // noop
+    }
+
+    public static void report(Throwable throwable) {
+        // noop
+    }
+}
